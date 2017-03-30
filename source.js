bc43746d2144da74da39d70316f3370955309eb1
Alex Hsu <easyhappy71826@gmail.com>
2017-03-30 11:41:40 +0800

    filling trangle with method of scan-line

--- source.js
+++ source.js
@@ -67,7 +67,7 @@ var CubeEdges =
 
 
 // Camera position
-var CameraPos = {x: 0, y: 0, z: -20};
+var CameraPos = {x: 0, y: 0, z: -10};
 
 // Camera distortion
 var RatioConst = 300;
@@ -215,11 +215,95 @@ function RenderScene()
     	var Color = {R: (CubeFaces[i].i *50) % 255, G: (CubeFaces[i].i * 128) % 255, B: (CubeFaces[i].i * 200 % 255)}
 
        // Render the face by looking up our vertex list
-        RenderFillTriangle(PointA.x, PointA.y, PointB.x, PointB.y, PointC.x, PointC.y, 2, Color);
-        RenderTriangle(PointA.x, PointA.y, PointB.x, PointB.y, PointC.x, PointC.y, 2);
+        // RenderFillTriangle(PointA.x, PointA.y, PointB.x, PointB.y, PointC.x, PointC.y, 2, Color);
+        // RenderTriangle(PointA.x, PointA.y, PointB.x, PointB.y, PointC.x, PointC.y, 2);
+
+                // Render the face by looking up our vertex list
+        CustomFillTriangle(PointA.x, PointA.y, PointB.x, PointB.y, PointC.x, PointC.y, 2, Color);
+        // CustomFillTriangle(0, 4, 3, 0, 3, 8, 2, Color);
     
     }
 
 
 
+}
+
+
+// Our custom triangle filler function
+function CustomFillTriangle(x1, y1, x2, y2, x3, y3, width, color)
+{
+    // Create an array of our vertices (as tuples) for easier access
+    var Vertices = new Array({x:x1, y:y1}, {x:x2, y:y2}, {x:x3, y:y3});
+    
+    // Sort such that (x1, y1) is always the top most point (in height), then (x2, y2), and then (x3, y3)
+    Vertices.sort(function(a,b) { return a.y - b.y; });
+    
+    // Define our edges: 1 to 2, 1 to 3, 2 to 3
+    // Order is important here so that we maintain that the first point in our edge
+    // is *always* higher (i.e. closer towards 0)
+    var Edges = [{a:1, b:2}, {a:1, b:3}, {a:2, b:3}];
+    
+
+    var ArrayContent = '' ;
+    Vertices.forEach(function (eachAry){
+    	ArrayContent = ArrayContent + ' x : ' + eachAry.x + ' y : ' + eachAry.y + ','
+    });
+    // console.log('ArrayContent: ' + ArrayContent);
+    // Find the top and bottom most point
+    // Note: since y grows positive top-to-bottom, we want the smallest value here
+    // Note: opposite logical implication for obtaining bottom position
+    // Final note: the data is already pre-sorted! Look a the first (topmost) and last (bottommost) vertices
+    var TopY = Vertices[0].y;
+    var BottomY = Vertices[2].y;
+    
+    // Pre-compute the slops and intersections of each edge, so that we can use this data as a look-up
+    // during the horizontal scan
+    var Slopes = new Array();
+    var Intercepts = new Array();
+    for(var i = 0; i < 3; i++)
+    {
+        // Find the edge vertices (-1 because our arrays start at 0)
+        var a = Edges[i].a - 1;
+        var b = Edges[i].b - 1;
+        
+        // Compute slope & edge
+        Slopes[i] = (Vertices[b].y - Vertices[a].y) / (Vertices[b].x - Vertices[a].x); // dy / dx
+        Intercepts[i] = Vertices[a].y - Slopes[i] * Vertices[a].x;
+
+        // console.log('Slope[' + i + ']:' + Slopes[i] + ' , Intercepts[' + i + ']:' + Intercepts[i]);
+    }
+    
+    // For each horizontal line..
+    for(var y = TopY; y <= BottomY; y++)
+    {
+        // Find our min x and max x (default to out of bounds to begin with)
+        var MinX = CanvasWidth + 1;
+        var MaxX = -1;
+        
+        // For each edge
+        for(var i = 0; i < 3; i++)
+        {
+            // Find the edge vertices (-1 because our arrays start at 0)
+            var a = Edges[i].a - 1;
+            var b = Edges[i].b - 1;
+            
+            // If we are in the range of this line, find the min/max
+            if(y >= Vertices[a].y && y <= Vertices[b].y)
+            {
+                // Compute the horizontal intersection
+                var x = (y - Intercepts[i]) / Slopes[i];
+                
+                // Save if new min or max values
+                MinX = Math.min(MinX, x);
+                MaxX = Math.max(MaxX, x);
+            }
+        }
+        
+        // Fill each pixel, using a line, for the given color
+        // Note: we fill 2 pixels wide because of an odd behavior of line-edges being anti-aliased
+        // which make the lines look transparent: switch to an edge of 1 to interesting behavior!
+        RenderLine(MinX, y, MaxX, y, 2, color);
+    }
+    
+    // Done rendering triangle
 }
